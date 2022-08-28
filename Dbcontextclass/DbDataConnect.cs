1a55a63a8e426f2459078b5f7f2f6cf8db7fc4cb
Your Name <you@example.com>
2022-08-28 20:09:45 +0600

    Add project files.

---
+++ Dbcontextclass/DbDataConnect.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dbcontextclass {
+    static public class DbDataConnect {
+        static public List<string> TableNames = new List<string>(new string[] {"Countries","Cities","Olympiads","Sports","SportTypes","Players","CProfiles","Medals"});
+        static public List<string> SelectNames = new List<string>(new string[] { "show all", "show all winners", "show all countries with gold", "show the most frequent owner country", "show the team of a single country", "show the winners of an olympiad", "show all countries with gold of an olympiad","show the contenstant with the most of gold in a sport","show all medals of an olympiad","show statistics of a country","show statisticsa of a county in an olympiad" });
+
+        static public List<int> IDExpressions = new List<int>();
+        static public List<string> AllSelects = new List<string>(new string[] { 
+            @"SELECT * FROM T_Countries INNER JOIN T_Players ON T_Countries.ID=T_Players.CountryID
+              INNER JOIN T_CProfiles ON T_CProfiles.PlayerID = T_Players.ID
+              INNER JOIN T_Sports ON T_CProfiles.SportID = T_Sports.ID
+              INNER JOIN T_SportTypes ON T_Sports.SportTypeID=T_SportTypes.ID
+              INNER JOIN T_Medals ON T_Medals.ProfileID = T_CProfiles.ID;",
+
+
+            @"SELECT T_Players.FirstName, T_PLayers.LastName , T_Medals.Rarity FROM T_Olympiads
+            INNER JOIN T_Sports ON T_Olympiads.ID=T_Sports.OlympiadID
+            INNER JOIN T_SportTypes ON T_SportTypes.ID = T_Sports.SportTypeID
+            INNER JOIN T_CProfiles ON T_CProfiles.SportID = T_Sports.ID
+            INNER JOIN T_Players ON T_Players.ID = T_CProfiles.PlayerID
+            INNER JOIN T_Medals ON T_Medals.ProfileID = T_CProfiles.ID
+            GROUP BY T_Players.FirstName, T_PLayers.LastName , T_Medals.Rarity;",
+
+
+            @"SELECT  T_Countries.CountryName,COUNT(*) AS 'Medal Count' FROM T_Countries
+            INNER JOIN T_Players ON T_Countries.ID = T_Players.CountryID
+            INNER JOIN T_CProfiles ON T_CProfiles.PlayerID = T_Players.ID
+            INNER JOIN T_Sports ON T_CProfiles.SportID = T_Sports.ID
+            INNER JOIN T_Olympiads ON T_Olympiads.ID = T_Sports.OlympiadID
+            INNER JOIN T_Medals ON T_Medals.ProfileID=T_CProfiles.ID
+            WHERE T_Medals.Rarity='Gold'
+            GROUP BY  T_Countries.CountryName",
+
+
+
+
+            @"SELECT TOP(1) T_Countries.CountryName ,COUNT(*) AS 'Owner Count' FROM T_Countries
+            INNER JOIN T_Cities ON T_Countries.ID = T_Cities.CountryID
+            INNER JOIN T_Olympiads ON T_Cities.ID= T_Olympiads.CityID
+            GROUP BY T_Countries.CountryName
+            ORDER BY [Owner Count] DESC",
+
+            @"SELECT *  FROM T_Countries
+            INNER JOIN T_Players ON T_Players.CountryID = T_Countries.ID
+            WHERE T_Countries.ID=0",
+
+
+
+            @"SELECT T_Players.ID,T_Players.FirstName, T_PLayers.LastName , T_Medals.Rarity FROM T_Olympiads
+            INNER JOIN T_Sports ON T_Olympiads.ID=T_Sports.OlympiadID
+            INNER JOIN T_SportTypes ON T_SportTypes.ID = T_Sports.SportTypeID
+            INNER JOIN T_CProfiles ON T_CProfiles.SportID = T_Sports.ID
+            INNER JOIN T_Players ON T_Players.ID = T_CProfiles.PlayerID
+            INNER JOIN T_Medals ON T_Medals.ProfileID = T_CProfiles.ID
+            WHERE T_Olympiads.ID=0
+            GROUP BY T_Players.ID,T_Players.FirstName, T_PLayers.LastName , T_Medals.Rarity;",
+
+            @"SELECT  T_Countries.CountryName,COUNT(*) AS 'Medal Count' FROM T_Countries
+            INNER JOIN T_Players ON T_Countries.ID = T_Players.CountryID
+            INNER JOIN T_CProfiles ON T_CProfiles.PlayerID = T_Players.ID
+            INNER JOIN T_Sports ON T_CProfiles.SportID = T_Sports.ID
+            INNER JOIN T_Olympiads ON T_Olympiads.ID = T_Sports.OlympiadID
+            INNER JOIN T_Medals ON T_Medals.ProfileID=T_CProfiles.ID
+            WHERE T_Olympiads.ID=0 AND T_Medals.Rarity='Gold'
+            GROUP BY  T_Countries.CountryName",
+
+
+            @"SELECT T_Players.ID,T_Players.FirstName,T_Players.LastName,COUNT(*) AS'Gold medal count' FROM T_Players
+            INNER JOIN T_CProfiles ON T_Players.ID=T_CProfiles.PlayerID
+            INNER JOIN T_Medals ON T_CProfiles.ID=T_Medals.ProfileID
+            INNER JOIN T_Sports ON T_CProfiles.SportID = T_Sports.ID
+            INNER JOIN T_SportTypes ON T_SportTypes.ID = T_Sports.SportTypeID
+            WHERE T_SportTypes.ID=0 AND T_Medals.Rarity='Gold'
+			GROUP BY T_Players.ID,T_Players.FirstName,T_Players.LastName",
+
+            @"SELECT * FROM T_Countries
+            INNER JOIN T_Players ON T_Countries.ID=T_Players.CountryID
+            INNER JOIN T_CProfiles ON T_CProfiles.PlayerID = T_Players.ID
+            INNER JOIN T_Sports ON T_CProfiles.SportID = T_Sports.ID
+            INNER JOIN T_Olympiads ON T_Olympiads.ID = T_Sports.OlympiadID
+            INNER JOIN T_SportTypes ON T_Sports.SportTypeID=T_SportTypes.ID
+            INNER JOIN T_Medals ON T_Medals.ProfileID = T_CProfiles.ID
+            WHERE T_Olympiads.ID = 0;",
+
+            @"SELECT * FROM T_Countries
+            INNER JOIN T_Players ON T_Countries.ID=T_Players.CountryID
+            INNER JOIN T_CProfiles ON T_CProfiles.PlayerID = T_Players.ID
+            INNER JOIN T_Sports ON T_CProfiles.SportID = T_Sports.ID
+            INNER JOIN T_Olympiads ON T_Olympiads.ID = T_Sports.OlympiadID
+            INNER JOIN T_SportTypes ON T_Sports.SportTypeID=T_SportTypes.ID
+            LEFT OUTER JOIN T_Medals ON T_Medals.ProfileID = T_CProfiles.ID
+            WHERE T_Countries.ID =0 ;",
+
+            $@"SELECT * FROM T_Countries
+            INNER JOIN T_Players ON T_Countries.ID=T_Players.CountryID
+            INNER JOIN T_CProfiles ON T_CProfiles.PlayerID = T_Players.ID
+            INNER JOIN T_Sports ON T_CProfiles.SportID = T_Sports.ID
+            INNER JOIN T_Olympiads ON T_Olympiads.ID = T_Sports.OlympiadID
+            INNER JOIN T_SportTypes ON T_Sports.SportTypeID=T_SportTypes.ID
+            LEFT OUTER JOIN T_Medals ON T_Medals.ProfileID = T_CProfiles.ID
+            WHERE T_Countries.ID =0 AND T_Olympiads.ID=1;"
+
+        });
+      private static DbConnection conn = null;
+      private static DbProviderFactory fact = null;
+      private static string connectionString = "";
+
+        public  async static Task<DataTable> GetTableById(int id,string constr) {
+            return await GetTableBySelect(AllSelects[id],constr);
+            
+        
+        
+        
+        }
+       static private void InitFactoryConn(string constr) { 
+
+            fact = DbProviderFactories.GetFactory("System.Data.SqlClient");
+            conn = fact.CreateConnection();
+            connectionString = constr;
+            conn.ConnectionString = connectionString;
+        
+        
+        }
+
+        public static string GetConnectionStringByProvider(string providerName) {
+            string returnValue = null;
+
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+
+            if (settings != null) {
+                foreach (ConnectionStringSettings cs in settings) {
+                    if (cs.ProviderName == providerName) {
+                        returnValue = cs.ConnectionString;
+                        break;
+                    }
+                }
+            }
+
+            return returnValue;
+
+        }
+        public async static Task<DataTable> GetTableBySelect(string selectCommand, string constr) {
+            InitFactoryConn(constr);
+            await conn.OpenAsync();
+
+
+            DbDataAdapter adapter = fact.CreateDataAdapter();
+            adapter.SelectCommand = conn.CreateCommand();
+            adapter.SelectCommand.CommandText = selectCommand;
+            await adapter.SelectCommand.ExecuteNonQueryAsync();
+
+            DataTable table = new DataTable();
+
+
+            adapter.Fill(table);
+
+
+            conn.Close();
+            
+            return table;
+
+
+
+        }
+    }
+}
---
+++ OlympiadApp/Form1.cs
@@ -0,0 +1,152 @@
+﻿using Dbcontextclass;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OlympiadApp {
+    public partial class Form1 : Form {
+         public  DataClasses1DataContext _dbcontext = new DataClasses1DataContext();
+        public Form1() {
+            InitializeComponent();
+        }
+
+
+         private void SetDataSourceByName(string name) {
+            switch (name) {
+
+                case "Countries": dataGridView1.DataSource= _dbcontext.T_Countries; break;
+                case "Cities": dataGridView1.DataSource = _dbcontext.T_Cities; break;
+                case "Olympiads": dataGridView1.DataSource = _dbcontext.T_Olympiads; break;
+                case "Sports": dataGridView1.DataSource = _dbcontext.T_Sports; break;
+                case "SportTypes": dataGridView1.DataSource = _dbcontext.T_SportTypes; break;
+                case "Players": dataGridView1.DataSource = _dbcontext.T_Players; break;
+                case "CProfiles": dataGridView1.DataSource = _dbcontext.T_CProfiles; break;
+                case "Medals": dataGridView1.DataSource = _dbcontext.T_Medals; break;
+                default: throw new ArgumentException();
+            }
+        
+        
+        
+        
+        }
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e) {
+            comboBox1.Items.Clear();
+            for(int i = 0; i < DbDataConnect.TableNames.Count; i++) {
+                comboBox1.Items.Insert(i,DbDataConnect.TableNames[i]);
+            }
+
+            comboBox3.Items.Clear();
+            for (int i = 0; i < DbDataConnect.SelectNames.Count; i++) {
+                comboBox3.Items.Insert(i, DbDataConnect.SelectNames[i]);
+            }
+
+
+            comboBox2.Items.Add("Alter");
+            comboBox2.Items.Add("Select");
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e) {
+            if (comboBox2.SelectedItem == "Alter") {
+
+                label1.Visible = true;
+                label3.Visible = true;
+                comboBox1.Visible = true;
+                button1.Visible = true;
+
+
+
+                label2.Visible = false;
+                comboBox3.Visible = false;
+                label5.Visible = false;
+                textBox1.Visible = false;
+                button2.Visible = false;
+            }
+            else if(comboBox2.SelectedItem == "Select") {
+                label2.Visible = true;
+                comboBox3.Visible = true;
+                label5.Visible= true;
+                textBox1.Visible = true;
+                button2.Visible = true;
+
+                label1.Visible = false;
+                label3.Visible = false;
+                comboBox1.Visible = false;
+                button1.Visible = false;
+
+
+            }
+
+
+            dataGridView1.DataSource = null;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            var val = comboBox1.SelectedItem.ToString ();
+            SetDataSourceByName(val);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            _dbcontext.SubmitChanges();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e) {
+
+
+
+
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e) {
+            int indx = comboBox3.SelectedIndex;
+            string select = DbDataConnect.AllSelects[indx];
+            if(indx > 3) {
+                var words = textBox1.Text.ToString().Split(' ');
+                for (int i = 0; i < words.Length; i++) {
+                    select = select.Replace(i.ToString(), words[i]);
+                }
+
+            }
+
+
+            dataGridView1.DataSource=await DbDataConnect.GetTableBySelect(select, DbDataConnect.GetConnectionStringByProvider("System.Data.SqlClient"));
+        }
+
+        private void label5_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
